3df3b25e0ea7ca0fde77797ce9d2ace212bf8504
Whitomtit <whitomtit@gmail.com>
2024-02-09 19:09:14 +0200

    Added dockerfile to make the service buildable into image

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div style={{ textAlign: "center", fontSize: "3rem", fontWeight: "bold", margin: "5rem"}}>
-      <h1 className="">Welcome to TekClinic's temporary website</h1>
+    <div style={{ textAlign: "center", fontSize: "3rem", fontWeight: "bold", margin: "5rem" }}>
+      <h1 className="">Welcome to TekClinic&apos;s temporary website</h1>
     </div>
   );
 }
