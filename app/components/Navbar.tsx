9480526eab73f51b9b2c7d7d6c5ec5a923e1dbf7
gottl <gottlieb.ofek@gmail.com>
2024-04-01 23:07:00 +0300

    logout inside navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,21 +1,44 @@
 import { AppShell, NavLink } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
 import { HomeIcon, PersonIcon} from '@modulz/radix-icons';
-
+import { AuthContext } from "../context/AuthContextProvider";
+import { useEffect, useState, useContext } from "react";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
+  const authContext = useContext(AuthContext);
+  const [username, setUsername] = useState<string>("");
+  const [error, setError] = useState<string | null>(null);
+
+  const [activeTable, setActiveTable] = useState('patient');
+  //console.log(authContext.keycloakToken);
+  const limit = 20;
+  const offset = 0;
+
+  const handleLogout = () => {
+    navigate('/'); // Navigate to home page
+    authContext.logout(); // Call logout function
+  };
+
+  useEffect(() => {
+    if (authContext.isAuthenticated && authContext.keycloakToken && authContext.username) {
+      setUsername(authContext.username);
+    }
+  }, [authContext.isAuthenticated, authContext.keycloakToken, authContext.username]);
+
+
   return (
-    <AppShell.Navbar p="md" style={{ gap: '10px' }}>
+    <AppShell.Navbar p="md" style={{ gap: '10px' , fontSize: '15px'}}>
         <NavLink
+            
             label="Home"
             onClick={() => navigate('/')}
-            style={{ margin: '5px' }}
+            style={{ margin: '5px'}}
         />
 
       <NavLink
-        label="Patients"
+        label=" Patients"
         onClick={() => navigate('/patients')}
         style={{ margin: '5px' }}
       />
@@ -24,6 +47,20 @@ const Navbar = () => {
         onClick={() => navigate('/volunteers-doctors')}
         style={{ margin: '5px' }}
       />
+
+<div style={{ flex: '1' }}>
+        {/* Your main content here */}
+      </div>
+      {/* Footer */}
+      <footer style={{ textAlign: 'center', padding: '10px' , fontSize: '15px'}}>
+      <NavLink
+            
+            label="Logout"
+            onClick={handleLogout}
+            style={{ margin: '5px'}}
+        />
+
+      </footer>
     </AppShell.Navbar>
   );
 };
--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -1,27 +1,24 @@
-import React, {useContext} from 'react';
-import {Title, Button} from '@mantine/core';
-import {AuthContext} from "../context/AuthContextProvider";
-import {useEffect, useState} from "react";
-import styles from './general.module.css'; // Import CSS module for styling
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../context/AuthContextProvider'
 
-export default function Home() {
-    const authContext = useContext(AuthContext);
-    const [username, setUsername] = useState<string>("");
+export default function Home () {
+  const authContext = useContext(AuthContext)
+  const [username, setUsername] = useState<string>('')
 
+  useEffect(() => {
+    if (authContext.isAuthenticated && authContext.keycloakToken && authContext.username) {
+      setUsername(authContext.username)
+    }
+  }, [authContext.isAuthenticated, authContext.keycloakToken, authContext.username])
 
-    useEffect(() => {
-        if (authContext.isAuthenticated && authContext.keycloakToken && authContext.username) {
-            setUsername(authContext.username);
-        }
-    }, [authContext.isAuthenticated, authContext.keycloakToken, authContext.username]);
-
-    return (
-        <div className={styles.container}>
-            <center>
-                <Title>Hello, {username}</Title>
-                <br/>
-                <Button onClick={authContext.logout}>Logout</Button>
-            </center>
-        </div>
-    );
+  return (
+        <div>Hi!</div>
+        // <div className={styles.container}>
+        //   <center>
+        //   <Title>Hello, {username}</Title>
+        //   <br />
+        //   <Button onClick={authContext.logout}>Logout</Button>
+        //   </center>
+        // </div>
+  )
 }
