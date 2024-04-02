a88f7a017437827163ee5cc4a3a8d2273db774d0
Andrey Babyn <whitomtit@gmail.com>
2024-04-02 15:54:59 +0300

    Reverted deleted files in rebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,28 @@
-'use client';
-import MyApp from './MyApp';
-import AuthContextProvider from './context/AuthContextProvider';
-import { MantineProvider } from '@mantine/core';
-import '@mantine/core/styles.css';
-import '@mantine/dates/styles.css';
-import 'mantine-react-table/styles.css';
+'use client'
+import MyApp from './MyApp'
+import AuthContextProvider from './context/AuthContextProvider'
+import { MantineProvider } from '@mantine/core'
+import { registerLicense } from '@syncfusion/ej2-base'
+import '@mantine/core/styles.css'
+import '@mantine/dates/styles.css'
+import 'mantine-react-table/styles.css'
+import React from 'react'
 
-function App() {
+const syncfusionLicenseKey = process.env.NEXT_PUBLIC_SYNCFUSION_LICENSE_KEY
+if (syncfusionLicenseKey == null) {
+  throw new Error('NEXT_PUBLIC_SYNCFUSION_LICENSE_KEY is not set. ' +
+        'You probably forgot to set it in your .env.local file.')
+}
+registerLicense(syncfusionLicenseKey)
+
+function App (): React.JSX.Element {
   return (
-    <AuthContextProvider>
-      <MantineProvider>
-        <MyApp />
-      </MantineProvider>
-    </AuthContextProvider>
+        <AuthContextProvider>
+            <MantineProvider>
+                <MyApp/>
+            </MantineProvider>
+        </AuthContextProvider>
   )
 }
 
-export default App;
+export default App
