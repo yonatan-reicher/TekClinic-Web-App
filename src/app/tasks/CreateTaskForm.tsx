9b32bf718bb16deb446fda0f0d809ca108f21489
Yonatan Reicher <yony252525@gmail.com>
2025-02-19 10:30:40 +0200

    Update tasks/page's table and CreateTaskForm to use the correct fields

--- src/app/tasks/CreateTaskForm.tsx
+++ src/app/tasks/CreateTaskForm.tsx
@@ -1,57 +1,87 @@
 import React, { useState } from 'react'
-import { Button, TextInput } from '@mantine/core'
+import { Button, Checkbox, MantineColorScheme, NumberInput, Space, Textarea, TextInput } from '@mantine/core'
+import { useForm } from '@mantine/form'
+import { CreateModalProps } from '@/src/components/CustomTable'
+import { notEmptyValidator } from '@/src/utils/validation';
+import { Task } from '@/src/api/model/task';
+import { TaskBaseScheme } from '@/src/api/scheme';
+import { errorHandler } from '@/src/utils/error';
+import { toast } from '@/src/utils/toast';
 
-interface FormData {
-  name: string
-  doctor: string
-  patient: string
-}
-
-interface CreateTaskFormProps {
-  onFinish: (data: FormData) => void
-}
-
-export default function CreateTaskForm ({ onFinish }: CreateTaskFormProps): JSX.Element {
-  const [name, setName] = useState('')
-  const [doctor, setDoctor] = useState('')
-  const [patient, setPatient] = useState('')
-
-  function handleSubmit (e: React.FormEvent<HTMLFormElement>): void {
-    e.preventDefault()
-    onFinish({
-      name,
-      doctor,
-      patient
-    })
+const onSubmit =
+  ({ session, computedColorScheme, onSuccess }: CreateModalProps) =>
+  async (values: TaskBaseScheme) => {
+    // First we create a new task,
+    await errorHandler(
+      async () => await toast(
+        Task.create(values, session),
+        'Creating task...',
+        'Task created!',
+        'Failed to create task',
+        computedColorScheme,
+      ),
+      computedColorScheme,
+    )
+    // Then we tell everyone we're done!
+    await onSuccess()
   }
 
+export default function CreateTaskForm ({ session, computedColorScheme, onSuccess }: CreateModalProps): JSX.Element {
+  const form = useForm({
+    mode: 'uncontrolled',
+    validateInputOnBlur: true,
+    initialValues: {
+      title: '',
+      description: '',
+      complete: false,
+      patient_id: 0,
+      expertise: null,
+    },
+    validate: {
+      title: notEmptyValidator('Title is required'),
+    },
+  });
+
   return (
-    <form onSubmit={handleSubmit}>
-      {/* 1) Task Name */}
+    <form onSubmit={form.onSubmit(onSubmit({ session, computedColorScheme, onSuccess }))}>
       <TextInput
-        label="Task Name"
-        placeholder="e.g. Schedule a check-up"
-        value={name}
-        onChange={(e) => { setName(e.currentTarget.value) }}
+        label="Task Title"
+        placeholder="Schedule a check-up"
         required
+        key={form.key('title')}
+        {...form.getInputProps('title')}
       />
 
-      {/* 2) Doctor */}
-      <TextInput
-        label="Doctor"
-        placeholder="e.g. Dr. Alice"
-        value={doctor}
-        onChange={(e) => { setDoctor(e.currentTarget.value) }}
+      { /* TODO: Custom component that selects an existing patient by name */ }
+      <NumberInput
+        label="Patient id"
         required
+        key={form.key('patient_id')}
+        {...form.getInputProps('patient_id')}
       />
 
-      {/* 3) Patient */}
+      { /* TODO: Check suggest existing fields */ }
       <TextInput
-        label="Patient"
-        placeholder="e.g. Bob"
-        value={patient}
-        onChange={(e) => { setPatient(e.currentTarget.value) }}
-        required
+        label="Expertise"
+        placeholder="e.g. Eye"
+        key={form.key('expertise')}
+        {...form.getInputProps('expertise')}
+      />
+
+      { /* TODO: support right-to-left automatic detection? Notice, there is a native html attribute for this */ }
+      <Textarea
+        label="Description"
+        placeholder="Patient hurts on his right eye"
+        key={form.key('description')}
+        {...form.getInputProps('description')}
+      />
+
+      <Space h="md" />
+
+      <Checkbox
+        label={form.getValues().complete ? 'Complete' : 'Not complete'}
+        key={form.key('complete')}
+        {...form.getInputProps('complete')}
       />
 
       <Button type="submit" mt="md">
