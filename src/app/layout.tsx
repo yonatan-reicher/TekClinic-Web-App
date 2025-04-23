e26ddc3f18694383c59ebb0f136239132a725898
Yonatan Reicher <yony252525@gmail.com>
2025-04-23 11:05:16 +0300

    Fix syncfusion key handling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { registerLicense } from '@syncfusion/ej2-base'
-//registerLicense('ORg4AjUWIQA/Gnt2XVhhQlJHfV1dX2RWfFN0QHNYf1Rzd19FaUwgOX1dQl9mSX9RfkRiXH9deH1cQWQ=');
+registerLicense(requireBuildEnv('NEXT_PUBLIC_SYNCFUSION_LICENSE_KEY', process.env.NEXT_PUBLIC_SYNCFUSION_LICENSE_KEY))
 import '@mantine/core/styles.layer.css'
 import '@mantine/dates/styles.layer.css'
 import 'mantine-contextmenu/styles.layer.css'
