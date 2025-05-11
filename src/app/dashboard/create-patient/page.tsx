9be6a3400cb45fc0836b7fb26ee57e8148ea1199
Yonatan Reicher <yony252525@gmail.com>
2025-05-11 11:39:33 +0300

    Implement demo pages

--- src/app/dashboard/create-patient/page.tsx
+++ src/app/dashboard/create-patient/page.tsx
@@ -1,3 +1,21 @@
+import Link from 'next/link'
+import {
+    Button,
+    Title,
+    Stack,
+} from '@mantine/core'
+
+
 export default function CreatePatientPage () {
-    return 'aarstar'
+    return <Stack>
+        <Title ta="center">Create a Patient</Title>
+        {/* Here we might show the exact same form from the CreatePatient Modal
+        */}
+        <Link prefetch={true} href={{
+            pathname: "/dashboard/create-appointment",
+            query: { patient: "test-patient" }
+        }}>
+            <Button fullWidth>New appointment for a test patient</Button>
+        </Link>
+    </Stack>
 }
--- src/app/dashboard/view-patient/page.tsx
+++ src/app/dashboard/view-patient/page.tsx
@@ -1,15 +1,28 @@
 'use client'
 
 
+import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
+import {
+    Button,
+    Title,
+    Stack,
+} from '@mantine/core'
 
 
 export default function Page() {
     const params = useSearchParams()
     const appointment = params.get("appointment")
     const patient = params.get("patient")
-    return <div>
-        <h4>Appointment: {appointment}</h4>
-        <h4>Patient: {patient}</h4>
-    </div>
+    return <Stack>
+        <Title>Appointment for Patient</Title>
+        <Title order={4}>Appointment: {appointment}</Title>
+        <Title order={4}>Patient: {patient}</Title>
+        <Link prefetch={true} href={{
+            pathname: "/dashboard/end-appointment",
+            query: { patient, appointment }
+        }}>
+            <Button>End Appointment</Button>
+        </Link>
+    </Stack>
 }
