cbb1b64b30121d44588fafa97f107b629061b9ba
Yonatan Reicher <yony252525@gmail.com>
2025-05-18 08:10:14 +0300

    Add Improve AppointmentSchedule

---
+++ src/components/AppointmentLine.tsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react'
+import {
+    Button,
+    Group,
+    Text,
+    useComputedColorScheme
+} from '@mantine/core'
+import { Appointment } from '@/src/api/model/appointment'
+import Date from '@/src/components/Date'
+import TimeRange from '@/src/components/TimeRange'
+import { useGuaranteeSession } from '@/src/utils/auth'
+import { errorHandler } from '@/src/utils/error'
+
+
+export interface AppointmentLineStyle {
+    hideDate?: boolean
+    hideTime?: boolean
+    hideDoctor?: boolean
+    hidePatient?: boolean
+}
+
+
+function If(props: { cond: boolean, children: React.ReactNode }) {
+    return props.cond ? <>{props.children}</> : null
+}
+
+
+// TODO: Add color based on the doctor... or something.
+function AppointmentLine(
+    {
+        appointment,
+        onClick,
+        hideDate,
+        hideTime,
+        hideDoctor,
+        hidePatient,
+    }: {
+        appointment: Appointment,
+        onClick?: () => Promise<void>,
+    } & AppointmentLineStyle
+) {
+    const session = useGuaranteeSession();
+    const ccs = useComputedColorScheme()
+    const { start_time: startTime, end_time: endTime } = appointment
+
+    const [doctorName, setDoctorName] = useState<string | null>(null)
+    const [patientName, setPatientName] = useState<string | null>(null)
+
+    // Need to load the doctor and patient to read it's name.
+    useEffect(() => {
+        errorHandler(async () => {
+            await Promise.all([
+                appointment.loadDoctor(session).then(() => {
+                    setDoctorName(appointment.getDoctorName())
+                }),
+                appointment.loadPatient(session).then(() => {
+                    setPatientName(appointment.getPatientName())
+                }),
+            ])
+        }, ccs)
+    }, [appointment, session])
+
+    // Paper is like a div with a shadow and a border.
+    // TODO: What if the end_timee is on a different day?
+    // return <Paper
+    //     shadow="md"
+    //     mb="xs"
+    //     bg="green"
+    //     w="100%"
+    //     display="inline-block"
+    //     pl="xs"
+    //     pr="xs"
+    //     c="black"
+    // >
+    return (
+        <Button
+            fullWidth
+            variant="subtle"
+            size="compact-md"
+            onClick={() => onClick ? onClick() : Promise.resolve()}>
+        <Group>
+            <If cond={!hideDate}><Date date={startTime} /></If>
+            <If cond={!hideTime}>
+                <TimeRange start={{date: startTime}} end={{date: endTime}}/>
+            </If>
+            <If cond={!hideDoctor}>
+                <Text c="white" fw="bold" m="auto">
+                    {patientName ?? 'Loading patient...'}
+                </Text>
+            </If>
+            <If cond={!hidePatient}>
+                <Text c="white" fw="bold" m="auto">
+                    {doctorName ?? 'Loading doctor...'}
+                </Text>
+            </If>
+        </Group>
+        </Button>
+    )
+}
+
+
+export default AppointmentLine
--- src/components/AppointmentSchedule.tsx
+++ src/components/AppointmentSchedule.tsx
@@ -1,15 +1,25 @@
-import { useState } from 'react';
-import { Stack, useComputedColorScheme } from '@mantine/core';
+import { useEffect, useState } from 'react';
+import {
+    Title,
+    Stack,
+    Text,
+    useComputedColorScheme,
+} from '@mantine/core';
 import { Appointment } from '@/src/api/model/appointment';
+import AppointmentLine, { AppointmentLineStyle } from '@/src/components/AppointmentLine'
 import { useGuaranteeSession } from '@/src/utils/auth';
 import { errorHandler } from '@/src/utils/error';
 
 
-export function AppointmentSchedule(props: {
-    date: Date | 'today', // This might change
-    doctor_id?: number,
-    patient_id?: number,
-}) {
+export default function AppointmentSchedule(
+    props: {
+        date: Date | 'today', // This might change
+        doctor_id?: number,
+        patient_id?: number,
+        onClick?: (_: Appointment) => Promise<void>,
+    // Maybe we can just infer these next properties automatically but whatever
+    } & AppointmentLineStyle
+) {
     const session = useGuaranteeSession();
     const ccs = useComputedColorScheme();
     const date = props.date === 'today' ? new Date() : props.date;
@@ -17,20 +27,35 @@ export function AppointmentSchedule(props: {
     // Get an array of appointments for the given date
     // TODO: Does this actually return every appointment, or is it limited to
     // some upper count?
-    const [appointments, setAppointments] = useState<Appointment[]>([]);
-    errorHandler(async () => {
-        const { items } = await Appointment.get({
-            date,
-            doctor_id: props.doctor_id,
-            patient_id: props.patient_id,
-        }, session);
-        setAppointments(items);
-    }, ccs)
+    const [appointments, setAppointments] = useState<Appointment[] | 'loading'>('loading');
+    useEffect(() => {
+        errorHandler(async () => {
+            const { items } = await Appointment.get({
+                date,
+                doctor_id: props.doctor_id,
+                patient_id: props.patient_id,
+            }, session);
+            setAppointments(items);
+        }, ccs)
+    }, [session])
 
     return (
-        <Stack>
-            Today's Appointments
-            {...appointments.map((appointment) => appointment.patient_id)}
+        <Stack gap="xs">
+            <Title order={4} ta="center">Today's Appointments</Title>
+            { appointments === 'loading'
+                ? <Text>Loading...</Text>
+                : <>{...appointments.map(appointment => {
+                    const onClick = () =>
+                        props.onClick
+                            ? props.onClick(appointment)
+                            : Promise.resolve()
+                    return <AppointmentLine
+                        {...props}
+                        appointment={appointment}
+                        onClick={onClick}
+                    />
+                })}</>
+            }
         </Stack>
     );
 }
